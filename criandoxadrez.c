cc3d4dab30400cf12025476da65d5d15ae169b38
Joniel44 <joniel_25@hotmail.com>
2025-08-01 00:10:16 +0000

    criando

---
+++ criandoxadrez.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+   
+   int torre = 5, bispo = 5, rainha = 8;
+   int contadorBispo = 0;
+   int contadorRainha = 0;
+
+   
+   for (int i = 0; i < torre; i++) {
+      printf("Torre 5 casas a frente!\n");
+   } 
+   printf("Bispo\n");
+   while (contadorBispo < bispo) {
+      printf("Diagonal 5 casas a Cima Direita\n");
+      contadorBispo++;
+   }
+   printf("Rainha\n");
+   
+   do {
+      printf(" 8 casas a Esquerda\n");
+      contadorRainha++;
+   } while (contadorRainha < rainha);     
+
+  
+  return 0;
+    
+}
