2c3e417670f000bb62ea2409d7e15dfab5123263
Joniel44 <joniel_25@hotmail.com>
2025-08-02 01:05:28 +0000

    xadrez aventureiro

--- criandoxadrez.c
+++ criandoxadrez.c
@@ -3,7 +3,7 @@
 int main() {
    
    int torre = 5, bispo = 5, rainha = 8;
-   int contadorBispo = 0, contadorRainha = 0;
+   int contadorBispo = 0, contadorRainha = 0, contadorCavalo = 1;
    int i, j;
 
    printf("**Jogo de Xadrez**\n\n");
@@ -24,7 +24,19 @@ int main() {
    do {
       printf("Esquerda\n");
       contadorRainha++;                      // A rainha seguindo 8 casas a esquerda
-   } while (contadorRainha < rainha);     
+   } while (contadorRainha < rainha); 
+   
+   printf("\nCavalo\n");
+
+  for (i = 0; i < contadorCavalo; i++)                // Utilizado um loop dentro do loop.
+   {  j = 0;
+   while (j < 2) {
+      printf("baixo\n");
+       j++;
+                           // Cavalo anda em L, seria duas linhas abaixo e uma coluna a esquerda.
+   }
+   printf("Esquerda\n");
+  }
 
   
   return 0;
